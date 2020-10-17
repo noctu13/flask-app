d6c3b4c7cb976a4948e0c91754a8438e1e2404cf
noctu13 <13bit57@gmail.com>
2020-10-17 18:20:55 +0300

    tests-fix

--- tests.py
+++ tests.py
@@ -3,13 +3,16 @@ import hello as tested_app
 
 
 class FlaskAppTests(unittest.TestCase):
+
     def setUp(self):
         tested_app.app.config['TESTING'] = True
         self.app = tested_app.app.test_client()
 
+
     def test_get(self):
         r = self.app.get('/')
         self.assertEqual(r.data.decode('utf-8'), 'I got it!')
+
 
     def test_post(self):
         r = self.app.post('/')
@@ -18,4 +21,3 @@ class FlaskAppTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
- 
